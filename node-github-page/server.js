3027df5b0ea619624fafe8f27f7a233757b5c50a
Alejandro Herrera <alejandro5herreraq@gmail.com>
2024-09-13 10:59:15 -0500

    mi primera versión del servidor

---
+++ node-github-page/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Servir archivos estáticos
+app.use(express.static('public'));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
